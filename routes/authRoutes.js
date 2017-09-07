cf677e2fac86d14cd27f4cf5b83201f4fce58143
bennyboy23 <b.j.e.evans-12@alumni.lboro.ac.uk>
2017-09-07 23:28:40 +0100

    added logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 const passport = require('passport');
 module.exports = (app) => {
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
 
-app.get('/auth/google/callback', passport.authenticate('google'));
-}
+    app.get('/auth/google/callback', passport.authenticate('google'));
+
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send('you are signed out: ' + req.user)
+    })
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    })
+};
