4c7f6b6f479cd46a27ceb7959d1ad61cecba4fe7
bennyboy23 <b.j.e.evans-12@alumni.lboro.ac.uk>
2017-09-07 22:49:11 +0100

    checks for existing user

--- services/passport.js
+++ services/passport.js
@@ -1,14 +1,23 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-    console.log('access token',accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile',profile);
-    console.log('done', done);
+    User
+        .findOne({googleId: profile.id})
+        .then((existingUser) => {
+            if (existingUser) {
+                //we know who you are
+            } else {
+                //need to create a new user
+                new User({googleId: profile.id}).save();
+            }
+        })
+
 }));
