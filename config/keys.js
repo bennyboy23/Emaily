4be0d73323120d73cd28c8cff3d55644d7ceeda0
Benjy <benjaminjosephevans1@gmail.com>
2017-09-08 15:07:42 +0100

    changed ignore

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+//figure out the correct credentials to return
+
+if(process.env.NODE_ENV === 'production'){
+//return prod keys
+module.exports = require('./prod');
+
+}else{
+//return dev keys
+    moduel.exports = require('./dev');
+
+}
