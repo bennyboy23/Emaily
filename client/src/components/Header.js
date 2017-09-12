d094e6586375a676f403fce7f3042b1105c4dafb
bennyboy23 <b.j.e.evans-12@alumni.lboro.ac.uk>
2017-09-12 18:50:16 +0100

    credits working

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,45 +1,54 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import Payments from './Payments';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
-    renderContent(){
-        switch (this.props.auth) {
-            case null:
-            return ;   
-            case false:
-            return (
-                <li><a href="/auth/google">Login with google</a></li>
-            );
-            default:
-            return [
-                <li key="1"><Payments /></li>,
-            <li key="2"><div className="chip red"><a href="/api/logout">Logout</a></div></li>
-        ];
-        }
-    }
-
-
-    render() {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
         return (
-            <nav className="nav-wrapper red">
-                <Link 
-                to={this.props.auth ? '/surveys' : '/'} 
-                className="brand-logo left">
-                    Emaily
-                </Link>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    {this.renderContent()}
-                </ul>
-            </nav>
+          <li>
+            <a href="/auth/google">Login with google</a>
+          </li>
         );
+      default:
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="3">Credits: {this.props.auth.credits}</li>,
+          <li key="2" style={{ padding: "0 10px 0 10px" }}>
+            <div className="chip red">
+              <a href="/api/logout">Logout</a>
+            </div>
+          </li>
+        ];
     }
-}
-function mapStateToProps({auth}){
-    return {
-        auth
-    }
-};
+  }
 
-export default connect(mapStateToProps) (Header);
+  render() {
+    return (
+      <nav className="nav-wrapper red">
+        <Link
+          to={this.props.auth ? "/surveys" : "/"}
+          className="brand-logo left"
+        >
+          Emaily
+        </Link>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          {this.renderContent()}
+        </ul>
+      </nav>
+    );
+  }
+}
+function mapStateToProps({ auth }) {
+  return {
+    auth
+  };
+}
+
+export default connect(mapStateToProps)(Header);
--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const coookieSession = require('cookie-session');
-const passport = require('passport');
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
+const express = require("express");
+const coookieSession = require("cookie-session");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const bodyParser = require("body-parser");
+require("./models/User");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(
-    coookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
+  coookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
